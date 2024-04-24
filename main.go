3b3a15a9ba1c706d6032e03d1fab85b2af14946e
Lukas Rastinger <lukas.rastinger@gmail.com>
2024-04-24 21:02:44 +0200

    Completes step 4.

--- main.go
+++ main.go
@@ -10,27 +10,48 @@ import (
 
 func main() {
 	numberFlag := flag.Bool("n", false, "Prints the line numbers.")
+	// lineBreakNums := flag.Bool("b", false, "Don't print line numbers for empty lines.")
 	flag.Parse()
 
-	for _, arg := range flag.Args() {
-
+	switch len(flag.Args()) {
+	case 0:
+		filePrinter(os.Stdin, *numberFlag)
+	case 1:
 		var input io.Reader
-
-		if arg != "-" {
-			file, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "rcat: could not open file: ")
-				os.Exit(1)
-			}
+		if flag.Arg(0) != "-" {
+			file := fileSource(flag.Arg(0))
 			defer file.Close()
-
 			input = file
 		} else {
 			input = os.Stdin
 		}
+		filePrinter(input, *numberFlag)
+	default:
+		for _, arg := range flag.Args() {
+			file := fileSource(arg)
+			defer file.Close()
+			filePrinter(file, *numberFlag)
+		}
+	}
+}
 
-		scanner := bufio.NewScanner(input)
-		for scanner.Scan() {
+func fileSource(pathToFile string) *os.File {
+	file, err := os.Open(pathToFile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "rcat: could not open file: ")
+		os.Exit(1)
+	}
+	return file
+}
+
+func filePrinter(input io.Reader, printLineNumbers bool) {
+	scanner := bufio.NewScanner(input)
+	lineCounter := 0
+	for scanner.Scan() {
+		lineCounter++
+		if printLineNumbers {
+			fmt.Fprintln(os.Stdout, lineCounter, " ", scanner.Text())
+		} else {
 			fmt.Fprintln(os.Stdout, scanner.Text())
 		}
 	}
